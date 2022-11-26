7a4ec8e39cd2fa74a89927e4f2af4e5a7516677d
suresh <90215118+skban@users.noreply.github.com>
2022-11-26 13:42:59 +0100

    Add files via upload

---
+++ code_4_1.py
@@ -0,0 +1,14 @@
+#Input  : data D  possible_instances
+#Output : logical expressions H.
+def LGGSet(d):
+    j=0
+    instances = d
+    instances_len = len(instances)
+    x= instances[j]
+    h=x
+    instances_left = instances_len -1
+    while j<instances_left:
+        j = j+1
+        x = possible_instances[j]
+        h = LGGConj(h,x)
+    return h
---
+++ code_4_2.py
@@ -0,0 +1,5 @@
+#Algorithm 4.2: LGG-Conj(x, y) – find least general conjunctive generalisation of two conjunctions.
+
+def LGGConj(x, y):
+    z= x & y
+    return z
